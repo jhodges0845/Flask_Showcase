7df30f26de6559a91e8a563069ce646bbe530b47
Jason Hodges <jasonhodges0845@gmail.com>
2020-04-19 19:26:16 -0500

    adding gunicorn, heroku needed files, and changed running port to port 80

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from APP import create_app
 
 app = create_app()
 
-#Run Server... runing on Port 5000#
+#Run Server Debug... runing on Port 5000#
+#Run Server Production... runing on Port 80#
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=80)
