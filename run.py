1dfbdbe17a0128ac528cf704b07e33d6ac0704e5
Jason Hodges <jasonhodges0845@gmail.com>
2020-04-19 21:30:23 -0500

    More changes to run.py and Procfile to try to get Heroku to work with Windows setup

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from APP import create_app
 
-hodges-flask-showcase = create_app()
+app = create_app()
 
 #Run Server Debug... runing on Port 5000#
 #Run Server Production... runing on Port 80#
 if __name__ == '__main__':
-    hodges-flask-showcase.run(host='0.0.0.0', port='80')
+    app.run(host='0.0.0.0', port='80')
