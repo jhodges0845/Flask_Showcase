fea5929bfc18b48e383a9954c9a6fa6e27cb90b7
Jason Hodges <jasonhodges0845@gmail.com>
2020-04-19 21:24:04 -0500

    more changes to try to work with Heroku

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from APP import create_app
 
-app = create_app()
+hodges-flask-showcase = create_app()
 
 #Run Server Debug... runing on Port 5000#
 #Run Server Production... runing on Port 80#
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    hodges-flask-showcase.run(host='0.0.0.0', port='80')
