3052bc7b61d35bc434b9a64d5a334a1f0dc042b0
Jason Hodges <jasonhodges0845@gmail.com>
2020-04-20 21:15:43 -0500

    In run.py changed from running on port 80 to run on default 5000

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()
 #Run Server Debug... runing on Port 5000#
 #Run Server Production... runing on Port 80#
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='80')
+    app.run(host='0.0.0.0')
